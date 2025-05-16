f608d8c79c6f8ae9ea6744f9f717f521abb9bab2
KIMSG <yuiox17@gmail.com>
2025-05-16 14:15:07 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+  render (<App/>);
+
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(minusButtonElement).toHaveTextContent("-");
+})
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
+})
+
+test('when the + button is pressed, the counter change', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+
+})
+
+
+test('when the - button is pressed, the counter change', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
+
+})
+
+test('on/off button has', () => {
+  render(<App/>);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+
+})
+
+test("prevent the -,+ button beling pressed", () => {
+  render(<App/>);
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
+
+})
